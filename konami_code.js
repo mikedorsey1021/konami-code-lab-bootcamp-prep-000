2def5789b7bcedc1eec3f776d1f47460e68bd429
mikedorsey1021@gmail.com dorsey <mikedorsey1021@gmail.com>
2018-04-17 16:06:08 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,7 +4,7 @@ function init() {
   // Write your JavaScript code inside the init() function
   let index = 0;
 
-  const input = document.querySelector('#mocha');
+  const input = document.body.querySelector('#mocha');
 
     input.addEventListner('keydown', function (e){
     const key = parseInt(e.detail || e.which);
