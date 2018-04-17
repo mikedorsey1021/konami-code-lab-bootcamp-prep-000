e620338742bab3b222816a5556d57d88a70ec22f
mikedorsey1021@gmail.com dorsey <mikedorsey1021@gmail.com>
2018-04-17 15:46:06 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,7 +4,7 @@ function init() {
   // Write your JavaScript code inside the init() function
   let index = 0;
 
-  document.body.addEventListner('click', function (e){
+  document.addEventListner('click', function (e){
     const key = parseInt(e.detail || e.which);
 
     if (key === code[index]){
